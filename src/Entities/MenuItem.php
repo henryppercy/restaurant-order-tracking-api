626d6418145052549b6e56198776c3725415bfe4
phone <pn330huboo@gmail.com>
2022-12-12 16:21:45 +0000

    return json for MenuItem

--- src/Entities/MenuItem.php
+++ src/Entities/MenuItem.php
@@ -2,7 +2,73 @@
 
 namespace App\Entities;
 
-class MenuItem
+class MenuItem implements \JsonSerializable
 {
+    private int $id;
+    private string $menuItemName;
+    private string $menuItemDesc;
+    private float $menuItemPrice;
+    private string $menuItemImageUrl;
+    private string $menuItemImageAlt;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMenuItemName(): string
+    {
+        return $this->menuItemName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMenuItemDesc(): string
+    {
+        return $this->menuItemDesc;
+    }
+
+    /**
+     * @return float
+     */
+    public function getMenuItemPrice(): float
+    {
+        return $this->menuItemPrice;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMenuItemImageUrl(): string
+    {
+        return $this->menuItemImageUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMenuItemImageAlt(): string
+    {
+        return $this->menuItemImageAlt;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->getId(),
+            'name' => $this->getMenuItemName(),
+            'description' => $this->getMenuItemDesc(),
+            'price' => $this->getMenuItemPrice(),
+            'imageURL' => $this->getMenuItemImageUrl(),
+            'imageAlt' => $this->getMenuItemImageAlt()
+        ];
+    }
 
 }
