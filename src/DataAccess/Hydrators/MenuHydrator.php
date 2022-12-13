81a298d4a6873a5f4fffa8e0065fc5693dfe9645
phone <pn330huboo@gmail.com>
2022-12-13 12:19:09 +0000

    getting json encode error

--- src/DataAccess/Hydrators/MenuHydrator.php
+++ src/DataAccess/Hydrators/MenuHydrator.php
@@ -2,7 +2,25 @@
 
 namespace App\DataAccess\Hydrators;
 
+use App\DataAccess\Database;
+use App\Entities\MenuItem;
+use App\Entities\MenuItemColl;
+
 class MenuHydrator
 {
+    public function hydrateMenuFromDb(Database $db, MenuItemColl $menuItemColl): MenuItemColl
+    {
+        $sql = 'SELECT `id`, `menu_item_name` as `menuItemName`, `menu_item_desc` as `menuItemDesc`,
+       `menu_item_price` as `menuItemPrice`,
+        `menu_item_image` as `menuItemImageUrl`, `image_alt` as `menuItemImageAlt` FROM `menu_items`;';
 
+        $pdo = $db->getConnection()->prepare($sql);
+        $pdo->execute();
+        $pdo->setFetchMode(\PDO::FETCH_CLASS, MenuItem::class);
+
+        $result = $pdo->fetchAll();
+
+        $menuItemColl->setMenuItemColl($result);
+        return $menuItemColl;
+    }
 }
