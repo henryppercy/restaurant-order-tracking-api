6aea339db2170b09984bf865f9160196b82f2f98
phone <pn330huboo@gmail.com>
2022-12-13 15:43:56 +0000

    added post route for creating new order

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Slim\App;
 use App\Controllers\GetMenuController;
+use App\Controllers\AddNewOrderController;
 
 return function (App $app) {
     $app->get('/menu', GetMenuController::class);
+    $app->post('/orders', AddNewOrderController::class);
 };
