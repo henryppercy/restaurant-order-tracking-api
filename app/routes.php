22342b443b721e0bdd690995cc8175755604b971
Henry <henryppercy@gmail.com>
2022-12-14 11:28:07 +0000

    Added route for if a route is not found

--- app/routes.php
+++ app/routes.php
@@ -3,25 +3,14 @@ declare(strict_types=1);
 
 use Slim\App;
 use App\Controllers\GetMenuController;
+use Slim\Exception\HttpNotFoundException;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
 
-    $app->options('/{routes:.+}', function ($request, $response, $args) {
-        return $response;
-    });
-
-    $app->add(function ($req, $res, $next) {
-        $response = $next($req, $res);
-        return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    });
-
     $app->get('/menu', GetMenuController::class);
 
-    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
-        $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
-        return $handler($req, $res);
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function (Request $request, $response): HttpNotFoundException {
+        throw new HttpNotFoundException($request);
     });
 };
