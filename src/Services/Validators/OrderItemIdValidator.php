32d4e6f35de57cdd36cf63c94e2cadbe0b9b3ee2
phone <pn330huboo@gmail.com>
2022-12-14 12:42:28 +0000

    Created validators for order number and menu item number

--- src/Services/Validators/OrderItemIdValidator.php
+++ src/Services/Validators/OrderItemIdValidator.php
@@ -2,7 +2,30 @@
 
 namespace App\Services\Validators;
 
+use App\DataAccess\DAO\ValidationDAO;
+use App\DataAccess\Database;
+
 class OrderItemIdValidator
 {
+    /**
+     * @var Database
+     */
+    private static Database $db;
 
+    /**
+     * @param Database $db
+     */
+    public function __construct(Database $db)
+    {
+        $this->db = $db;
+    }
+
+    public static function validateOrderItemNumber(int $menuItemNum): bool
+    {
+        if (ValidationDAO::checkIfExists(self::$db, $menuItemNum, '`menu_items`', '`id')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/Services/Validators/OrderNumberValidator.php
+++ src/Services/Validators/OrderNumberValidator.php
@@ -2,7 +2,16 @@
 
 namespace App\Services\Validators;
 
+use App\DataAccess\DAO\ValidationDAO;
+
 class OrderNumberValidator
 {
-
+    public static function validateOrderNumber(int $orderNum): bool
+    {
+        if (ValidationDAO::checkIfExists($orderNum, '`orders`', '`order_number_id')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
