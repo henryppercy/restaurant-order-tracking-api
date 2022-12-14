30c5ac9007c1b6a0ea326ca14579c7d570c56199
Henry <henryppercy@gmail.com>
2022-12-14 15:47:39 +0000

    Removed unused use statements

--- app/middleware.php
+++ app/middleware.php
@@ -2,9 +2,6 @@
 declare(strict_types=1);
 
 use Slim\App;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 return function (App $app) {
     $app->addBodyParsingMiddleware();
