dc6207a3d93375bba0126b94fdf4d7e6fed6b367
phone <pn330huboo@gmail.com>
2022-12-14 11:45:51 +0000

    adding route to enable new order item to be added

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Slim\App;
 use App\Controllers\GetMenuController;
+use App\Controllers\AddOrderItemController;
 
 return function (App $app) {
     $app->get('/menu', GetMenuController::class);
+    $app->post('orders/{orderNumber', AddOrderItemController::class);
 };
