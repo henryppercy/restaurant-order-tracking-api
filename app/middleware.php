bfe98fb078ccbd8d1153cc6b1ef88110d475b30f
Henry <henryppercy@gmail.com>
2022-12-14 11:27:38 +0000

    Added middleware to enable Cors

--- app/middleware.php
+++ app/middleware.php
@@ -2,8 +2,24 @@
 declare(strict_types=1);
 
 use Slim\App;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 return function (App $app) {
     $app->addBodyParsingMiddleware();
-//    $app->add(); // example of how to add Middleware
+
+    $app->options('/{routes:.+}', function (Request $request, Response $response, $args): Response
+    {
+        return $response;
+    });
+
+    $app->add(function (Request $request, RequestHandler $handler): Response
+    {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
 };
