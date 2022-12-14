9cd57ca45c7f5ecac5216f5bdfb68a4182c1cf45
phone <pn330huboo@gmail.com>
2022-12-14 11:53:16 +0000

    handling the order number from the endpoint and passing to OrderItemService

--- src/Controllers/AddOrderItemController.php
+++ src/Controllers/AddOrderItemController.php
@@ -2,7 +2,42 @@
 
 namespace App\Controllers;
 
-class AddOrderItemController
-{
+use App\Abstracts\Controller;
+use App\Services\OrderItemService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
+class AddOrderItemController extends Controller
+{
+    private OrderItemService $orderItemService;
+
+    /**
+     * @param OrderItemService $orderItemService
+     */
+    public function __construct(OrderItemService $orderItemService)
+    {
+        $this->orderItemService = $orderItemService;
+    }
+
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return void
+     */
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        $orderNumber = $args['orderNumber'];
+
+        $orderItemDetails = $request->getParsedBody();
+
+        $this->orderItemService->setOrderNumber($orderNumber);
+
+        $data = $this->orderItemService->postNewOrderItem($orderItemDetails);
+
+        $statusCode = $this->orderItemService->getStatusCode();
+
+        return $this->respondWithJson($response, $data, $statusCode);
+    }
 }
