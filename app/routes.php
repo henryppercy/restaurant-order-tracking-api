fc1f06d6daa2a3e30d2a17eae8f637113e83fd40
phone <pn330huboo@gmail.com>
2022-12-14 10:54:50 +0000

    enabling cors

--- app/routes.php
+++ app/routes.php
@@ -6,6 +6,22 @@ use App\Controllers\GetMenuController;
 
 return function (App $app) {
 
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
+
+    $app->add(function ($req, $res, $next) {
+        $response = $next($req, $res);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
+
     $app->get('/menu', GetMenuController::class);
 
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+        $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+        return $handler($req, $res);
+    });
 };
