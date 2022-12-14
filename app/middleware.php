b1c07e8ea5466d588191273cb289c6bc6266f2c0
phone <pn330huboo@gmail.com>
2022-12-14 12:49:20 +0000

    Removed uneccessary middleware

--- app/middleware.php
+++ app/middleware.php
@@ -8,18 +8,4 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 return function (App $app) {
     $app->addBodyParsingMiddleware();
-
-    $app->options('/{routes:.+}', function (Request $request, Response $response, $args): Response
-    {
-        return $response;
-    });
-
-    $app->add(function (Request $request, RequestHandler $handler): Response
-    {
-        $response = $handler->handle($request);
-        return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    });
 };
